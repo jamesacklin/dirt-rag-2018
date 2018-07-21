8db180b34e68e119014b53bc7cb808b5a0388657
James Acklin <james.acklin@gmail.com>
2018-07-21 09:12:47 -0400

    safer include

--- template-homepage.php
+++ template-homepage.php
@@ -2,6 +2,6 @@
 <?php $mh_magazine_options = mh_magazine_theme_options(); ?>
 <?php get_header(); ?>
 <div>
-	<?php include 'dist/wp-include.php' ?>
+	<?php include(dirname(__DIR__).'/dist/wp-include.php') ?>
 </div>
 <?php get_footer(); ?>
