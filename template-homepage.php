4ed153e49186531c3cb40646332f2c3471bcd1f9
James Acklin <james.acklin@gmail.com>
2018-08-09 15:34:24 -0400

    sidebar ads

--- template-homepage.php
+++ template-homepage.php
@@ -7,14 +7,28 @@
       <div id="main-content" class="mh-content mh-home-content">
         <div id="homepage-vue-app"></div>
       </div>
-      <?php if (is_active_sidebar('mh-home-6')) { ?>
+      <?php if ( !wp_is_mobile() ) { ?>
         <div class="mh-widget-col-1 mh-sidebar mh-home-sidebar mh-home-area-6">
-          <?php dynamic_sidebar('mh-home-6'); ?>
+          <div id='div-gpt-ad-1487038544877-0'></div>
+          <br>
+          <div id='div-gpt-ad-1487038544877-1'></div>
+          <br>
+          <div id='div-gpt-ad-1487038544877-2'></div>
+          <br>
+          <div id='div-gpt-ad-1487038544877-3'></div>
+          <br>
+          <div id='div-gpt-ad-1487038544877-4'></div>
+          <br>
+          <div id='div-gpt-ad-1487038544877-5'></div>
+          <br>
+          <div id='div-gpt-ad-1487038544877-6'></div>
+          <br>
+          <div id='div-gpt-ad-1487038544877-7'></div>
         </div>
       <?php } ?>
-      </div>
     </div>
   </div>
+</div>
 
 </div>
 <?php get_footer(); ?>
