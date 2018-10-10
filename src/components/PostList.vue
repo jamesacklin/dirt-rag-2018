b536c89d75f162a2518ec5c24453a7c669ce1eae
James Acklin <james.acklin@gmail.com>
2018-10-10 10:57:07 -0400

    posts now come from store

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -2,22 +2,22 @@
   <div class="mb3">
     <h4 class="mb3">
       <span class="">
-        <span v-if="!categoryName">&nbsp;</span>
-        <router-link :to="{ name: 'category', params: { cat_id: this.category } }" class="oswald db ttu pa2 bg-red white normal f4 no-underline hover-bg-black" v-if="categoryName">
-          <span v-html="categoryName"></span> <i class="fa fa-arrow-right fr"></i>
+        <span v-if="!$store.state.categoryDict[this.category]">&nbsp;</span>
+        <router-link :to="{ name: 'category', params: { cat_id: this.category } }" class="oswald db ttu pa2 bg-red white normal f4 no-underline hover-bg-black" v-if="$store.state.categoryDict[this.category]">
+          <span v-html="$store.state.categoryDict[this.category]"></span> <i class="fa fa-arrow-right fr"></i>
         </router-link>
       </span>
     </h4>
     <Spinner class="spinner" v-if="!postsLoaded" key="spinner"></Spinner>
-    <div class="post-list">
+    <div class="post-list" v-if="postsLoaded">
       <Post v-for="(post, index) in postArray" :key="index" :postdata="post" :index="index"></Post>
     </div>
     <div v-if="postsLoaded">
       <button class="input-reset action-button" v-if="counter < 6" v-on:click="increment">
-        <span class="">More {{ categoryName }} <i class="fa fa-chevron-down fr"></i> </span>
+        <span class="">More {{ $store.state.categoryDict[this.category] }} <i class="fa fa-chevron-down fr"></i> </span>
       </button>
       <router-link :to="{ name: 'category', params: { cat_id: this.category } }" class="action-button" v-if="counter >= 6">
-        <span class="">See All {{ categoryName }} <i class="fa fa-arrow-right fr"></i></span>
+        <span class="">See All {{ $store.state.categoryDict[this.category] }} <i class="fa fa-arrow-right fr"></i></span>
       </router-link>
     </div>
   </div>
@@ -36,36 +36,25 @@ export default {
   },
   data () {
     return {
-      posts: [],
-      categoryName: '',
       postsLoaded: false,
       counter: 3
     }
   },
   created () {
-    this.$store.dispatch('getWpCat', { category: this.category, perpage: 6 })
+    if (this.$store.state.posts[this.category]) {
+      this.postsLoaded = true
+    } else {
+      this.$store.dispatch('getWpCat', { category: this.category, perpage: 6 })
+      this.$store.subscribe((mutation, state) => {
+        if (mutation.type === 'storePosts') {
+          this.postsLoaded = true
+        }
+      })
+    }
   },
   computed: {
     postArray: function () {
-      var _this = this
-      this.$store.subscribe((mutation, state) => {
-        if (mutation.type === 'storePosts') {
-          // FIXME: Abstract this, never use local data. Reference the vuex store.
-          // Passing params to the child POST is OK.
-          _this.posts = state.posts[this.category]
-          _this.postsLoaded = true
-        }
-      })
-      this.$store.subscribe((mutation, state) => {
-        if (mutation.type === 'storeCats') {
-          // FIXME: Abstract this, never use local data. Reference the vuex store.
-          // Passing params to the child POST is OK.
-          _this.categoryName = state.categoryDict[this.category]
-        }
-      })
-      if (_this.posts) {
-        return this.posts.slice(0, this.counter)
-      }
+      return this.$store.state.posts[this.category].slice(0, this.counter)
     }
   },
   methods: {
